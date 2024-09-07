d62130b46a98792ba9d18c7202bc16c38cd77554
wgtrevillyan <wgtrevillyan@gmail.com>
2024-09-07 15:46:46 -0700

    Debugging Issue for Serverless Function Test where it works on local but not on vercel deployed app

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -3,5 +3,6 @@ export const runtime = 'nodejs'; // nodejs or deno
  
 export function GET(request: Request) {
   console.log('Time:', new Date().toISOString());
+  let url = new URL(request.url);
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
