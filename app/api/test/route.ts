c2ade54d1e1c526eb1de6314cd752e4bbb5cfcf2
wgtrevillyan <wgtrevillyan@gmail.com>
2024-09-07 13:01:59 -0700

    Testing commits for vercel

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,5 +1,6 @@
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
 export function GET(request: Request) {
+
   return new Response(`Hello from ${process.env.VERCEL_REGION}`);
 }
