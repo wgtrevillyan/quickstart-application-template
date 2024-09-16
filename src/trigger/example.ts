204cb442a12282df755011a06f0cda044f3288ce
wgtrevillyan <wgtrevillyan@gmail.com>
2024-09-15 23:28:09 -0700

    The whole syncing gmail messages process works locally. Now, trigger.dev needs to be set up so that it can run on production.

--- src/trigger/example.ts
+++ src/trigger/example.ts
@@ -1,4 +1,4 @@
-import { logger, task, wait } from "@trigger.dev/sdk/v3";
+import { logger, task } from "@trigger.dev/sdk/v3";
 import syncGmailMsgs from '../../services/sync/sync_gmail_msgs.mjs';
 
 
@@ -7,18 +7,25 @@ export const sync_gmail_msgs = task({
   run: async (payload: any, { ctx }) => {
     //logger.log("\n"); // Log a new line
     //logger.log(request.method, ' ', request.url, ' at time: ', new Date().toISOString()); // Log the request
+
+    if (ctx) {
+      logger.log("Context: ", ctx);
+    }
+    if (payload) {
+      logger.log("Payload: ", payload);
+    }
   
     try {
-      console.log("Starting sync service..."); // Log the message
+      logger.log("Starting sync service..."); // Log the message
   
       // Run the sync_gmail_msgs function
       await syncGmailMsgs.run();
   
-      console.log("Sync service completed."); // Log the message
+      logger.log("Sync service completed."); // Log the message
   
       return new Response("Sync service triggered."); // Return a response
     } catch (error) {
-      console.log("An error occurred when attempting to start the synce service:"); // Log the message
+      logger.log("An error occurred when attempting to start the synce service:"); // Log the message
       console.error(error); // Log the error
       return new Response("Error: " + error, { status: 500 }); // Return a response
     }
