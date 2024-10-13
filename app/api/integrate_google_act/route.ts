bbf73b762ec03aca96f80df266e7f57cbe2fad50
wgtrevillyan <wgtrevillyan@gmail.com>
2024-10-13 07:01:04 -0700

    Added a testing integrateGoogleAct api call to vercel deployment in an attempt to solve CORS error blocking api calls to vercel from flutterflow

--- app/api/integrate_google_act/route.ts
+++ app/api/integrate_google_act/route.ts
@@ -38,7 +38,8 @@ export async function POST(request: Request) {
     headers: {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      'Access-Control-Allow-Credentials': 'true'
     }
   });
 }
