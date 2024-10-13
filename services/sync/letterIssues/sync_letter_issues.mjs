532c01e3c829c1598df5b0182433c5f78dbc966b
wgtrevillyan <wgtrevillyan@gmail.com>
2024-10-12 18:54:16 -0700

    sync_letter_issues is working on local now

--- services/sync/letterIssues/sync_letter_issues.mjs
+++ services/sync/letterIssues/sync_letter_issues.mjs
@@ -4,14 +4,11 @@ import { connectToSupabaseClient } from "../../../lib/establish_clients.mjs";
 import { getEmailAccountIds } from "../../../lib/supabase_queries.mjs";
 import { getAuthId } from "../../../lib/supabase_queries.mjs";
 
-const supabaseClient = async () => await connectToSupabaseClient();
-
-const getApprovedSenderAddresses = async (userId, emailAccountId) => {
+const getApprovedSenderAddresses = async (supabaseClient, emailAccountId) => {
   try {
-    const { data, error } = await supabaseClient()
+    const { data, error } = await supabaseClient
       .from('senderAddresses')
       .select('senderAddress')
-      .eq('userId', userId)
       .eq('emailAccountId', emailAccountId)
       .eq('status', 'approved');
 
@@ -28,13 +25,13 @@ const getApprovedSenderAddresses = async (userId, emailAccountId) => {
   }
 };
 
-const getGmailMessageIds = async (senderAddresses) => {
+const getGmailMessageIds = async (supabaseClient, senderAddresses) => {
   try {
-    const { data, error } = await supabaseClient()
+    const { data, error } = await supabaseClient
       .from('gmailMessages')
       .select('id')
       .eq('syncedToIssues', false)
-      .in('senderAddress', senderAddresses);
+      .in('senderEmail', senderAddresses);
 
     if (error) {
       throw new Error(error.message);
@@ -49,9 +46,9 @@ const getGmailMessageIds = async (senderAddresses) => {
   }
 };
 
-const getMessageDetails = async (gmailMessageId) => {
+const getMessageDetails = async (supabaseClient, gmailMessageId) => {
   try {
-    const { data, error } = await supabaseClient()
+    const { data, error } = await supabaseClient
       .from('gmailMessages')
       .select('senderName, senderEmail, subject, bodySnippet, bodyText, bodyHtml, receivedAt')
       .eq('id', gmailMessageId);
@@ -61,7 +58,7 @@ const getMessageDetails = async (gmailMessageId) => {
     } else if (data === null) {
       throw new Error('Message details not retrieved. Returned data is null.');
     } else {
-      return data;
+      return data[0];
     }
   } catch (err) {
     console.error('Retrieval of message details failed. Unexpected error:', err);
@@ -69,9 +66,9 @@ const getMessageDetails = async (gmailMessageId) => {
   }
 };
 
-const storeMessageDetails = async (authId, emailAccountId, gmailMessageId, messageDetails) => {
+const storeMessageDetails = async (supabaseClient, authId, emailAccountId, gmailMessageId, messageDetails) => {
   try {
-    const { status, data, error, statusText } = await supabaseClient()
+    const { status, data, error, statusText } = await supabaseClient
       .from('letterIssues')
       .insert({
         authId: authId,
@@ -88,29 +85,25 @@ const storeMessageDetails = async (authId, emailAccountId, gmailMessageId, messa
 
     if (error) {
       throw new Error(error.message);
-    } else if (status !== 200) {
+    } else if (status !== 201) {
       throw new Error(`Message details not stored. Unexpected error: ${status} ${statusText}`);
     } else {
-      console.log("Message details stored successfully.");
       return true;
     }
   } catch (err) {
-    console.error('Storing of message details failed. Unexpected error:', err);
     return false;
   }
 };
 
-const markMsgAsSynced = async (emailAccountId, gmailMessageId) => {
+const markMsgAsSynced = async (supabaseClient, emailAccountId, gmailMessageId) => {
   try {
-    const { data, status, statusText, error } = await supabaseClient()
+    const { data, status, statusText, error } = await supabaseClient
       .from('gmailMessages')
       .update({ syncedToIssues: true})
       .eq('id', gmailMessageId);
 
     if (error) {
-      throw new Error(error.message);
-    } else if (status !== 200) {
-      throw new Error(`Sync status not updated. Unexpected error: ${status} ${statusText}`);
+      throw new Error(`Unexpected error occured when marking gmail message as synced: ${status} ${statusText} - ${error.message}`);
     } else {
       return true;
     }
@@ -128,47 +121,68 @@ export default {
 
     try {
 
+        // Connect to the Supabase client
+        const supabaseClient = await connectToSupabaseClient();
+
         const authId = await getAuthId(userId);
         if (!authId) {
             throw new Error('Auth user id not retrieved.');
         }
 
         // Get email account IDs
-        let issuesSynced = 0;
+        const emailAccounts = await getEmailAccountIds(userId);
+        console.log(`Email account IDs retrieved: ${emailAccounts[0].id}`);
+
+        // Get email account IDs
+        var issuesSynced = 0;
         let issueSyncErrors = 0;
-        for (let i = 0; i < emailAccountIds.length; i++) {
-          const senderAddresses = await getApprovedSenderAddresses(userId, emailAccountIds[i]);
-          if (!senderAddresses) {
-              console.error(`No approved sender addresses retrieved for email account: ${emailAccountsIds[i]}`);
+        var noApprovedErrors = 0;
+        for (let i = 0; i < emailAccounts.length; i++) {
+
+          console.log(`Syncing email account: ${emailAccounts[i].id}`);
+
+          // Retrieve the approved sender addresses
+          const unformattedSenderAddresses = await getApprovedSenderAddresses(supabaseClient, emailAccounts[i].id);
+          const senderAddresses = unformattedSenderAddresses.map(address => address.senderAddress);
+          if (!senderAddresses || senderAddresses.length === 0) {
+              console.error(`No approved sender addresses retrieved for email account: ${emailAccounts[i].id}`);
+              noApprovedErrors++;
               continue;
+          } else {
+              console.log(`Approved sender addresses retrieved: ${senderAddresses.length}`);
           }
 
           // Retrieve the gmail message ids
-          const gmailMessageIds = await getGmailMessageIds(senderAddresses);
-          if (!gmailMessageIds) {
-              console.error(`Gmail message ids not retrieved for email account: ${emailAccountsIds[i]}`);
+          const unformattedGmailMessageIds = await getGmailMessageIds(supabaseClient, senderAddresses);
+          const gmailMessageIds = unformattedGmailMessageIds.map(msg => msg.id);
+          if (!gmailMessageIds || gmailMessageIds.length === 0) {
+              console.error(`Gmail message ids not retrieved for email account: ${emailAccounts[i].id}`);
               continue;
+          } else {
+            console.log(`Gmail message ids retrieved: ${gmailMessageIds.length}`);
           }
 
           // Sync the messages
           for (let j = 0; j < gmailMessageIds.length; j++) {
             
+              if (j % 10 === 0) {console.log(`Syncing msg ${j} of ${gmailMessageIds.length}...\r`);}
+
               // Retrieve the message details
-              const messageDetails = await getMessageDetails(gmailMessageIds[j]);
-              if (!messageDetails) {
+              const messageDetails = await getMessageDetails(supabaseClient, gmailMessageIds[j]);
+              if (!messageDetails || messageDetails.length === 0) {
                   issueSyncErrors++;
                   continue;
               }
 
               // Store the message details
-              const stored = await storeMessageDetails(authId, emailAccountIds[i], gmailMessageIds[j], messageDetails);
+              const stored = await storeMessageDetails(supabaseClient, authId, emailAccounts[i].id, gmailMessageIds[j], messageDetails);
               if (!stored) {
                   issueSyncErrors++;
               } else {
                   // Mark the message as synced
-                  const msgMarked = await markMsgAsSynced(emailAccountIds[i], gmailMessageIds[j]);
+                  const msgMarked = await markMsgAsSynced(supabaseClient, emailAccounts[i].id, gmailMessageIds[j]);
                   if (!msgMarked) {
-                      issueSyncErrors++;
+                    issueSyncErrors++;
                   } else {
                     issuesSynced++;
                   }
@@ -176,15 +190,20 @@ export default {
           }
         }
 
-        if (issueStorageErrors > 0) {
-            throw new Error(`Failed to sync ${issueStorageErrors} gmail messages.`)
+
+        if (noApprovedErrors > 0) {
+            throw new Error(`Failed to sync ${noApprovedErrors} email accounts with no approved sender addresses.`);
+        } else if (issueSyncErrors > 0) {
+            throw new Error(`Failed to sync ${issueSyncErrors} gmail messages.`);
         } else if (issuesSynced === 0) {
-            console.warning('No issues were synced.')
-        } 
+            console.log('No issues were synced.');
+        } else {
+            console.log(`Synced ${issuesSynced} issues. `);
+        }
 
     } catch (e) {
-        console.error(e.message);
-        return { synced: false, issuesSynced: issuesSynced, error: error };
+        console.error('Error: ', e.message);
+        return { synced: false, issuesSynced: issuesSynced, error: e.message };
     }
 
     console.log("Sync service finished.");
